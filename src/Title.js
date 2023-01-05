0f55145a670075b080ed07d1abe858142e19beb9
Irshad Nazree <inazree370@gmail.com>
2023-01-05 11:53:57 +0800

    On branch master Your branch is up to date with 'Portfolio/master'. Changes committed: 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Title.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   src/style.css

---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+    <div className='text-center center'>
+        <h1>Page Under Development</h1>
+        <p>Currently Upgrading</p>
+    </div>
+    )
+}
